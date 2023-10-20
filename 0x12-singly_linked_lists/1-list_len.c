1fd1cbd7536cb3a67fa5d546bda0d9a885ab4e17
pauligram <paulgbekeloluwa@gmail.com>
2023-10-20 15:36:40 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,21 @@
+#include "lists.h"
+/*
+ * list_len - returns the numbers of elements in list_t
+ * @h: constant pointer type list_t
+ *
+ * Return: numbers of elements in list_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t na;
 
+	na = 0;
+
+	while (h)
+	{
+		na++;
+		h = h->next;
+	}
+
+	return (na);
+}
