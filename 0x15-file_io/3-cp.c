aa23832bc7d65cd3aca9411d559e9bd84561e5ec
pauligram <paulgbekeloluwa@gmail.com>
2023-10-31 03:53:15 +0100

    0x15-file.io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+
 
 /**
  * append_text_to_file 
@@ -6,28 +8,4 @@
  * @text_content: the NULL terminated
  * Return: 1 on success,
  */
-int append_text_to_file(cont char *filename, char *text_content)
-{
-	int file_descriptor, lenght = 0;
 
-	if (!filename)
-		return (-1)
-	file_descriptor = open(filename, O_RDWR| O_APPEND);
-	if (file_descriptor < 0)
-		return (-1)
-	if (! text_content)
-	{
-		close(file_descriptor);
-		return (-1);
-	}
-
-	while (text_content[length])
-		length++
-	if (write(file_descriptor, text_content, length) < 0) 
-	{
-		close(file_descriptor);
-		return (-1);
-	} 
-	close(file_descriptor);
-	retur (1);
-} 
