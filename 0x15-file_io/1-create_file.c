d43d19e155df54734b78007fb33640085b028dbb
pauligram <paulgbekeloluwa@gmail.com>
2023-10-31 03:32:10 +0100

    0x15-file.io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /*
- * create_file - create file
- * @filename: file
- * @text_content
- * Return: 1
+ * create_file - create a file
+ * @filename: A pointer to the name of the file to create
+ * @text_content: A pointer to the string to write to the file
+ * Return: If function fails - -1.
  */
-
-int append_text_to_file(const char *filename, char *m_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
 	int length = 0;
@@ -16,9 +15,9 @@ int append_text_to_file(const char *filename, char *m_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-while (m_content && m_content[length])
+while (text_content && text_content[length])
 	length++;
-if (write(fd, m_content, length) < 0)
+if (write(fd, text_content, length) < 0)
 {
 	close(fd);
 	return (-1);
